71418460a91999aa27c48dd45378a1694845fa7e
Do Anh Tu <tu0703@gmail.com>
2018-01-14 21:28:18 +0700

    convert tabs to spaces

--- xinh.today/src/App.js
+++ xinh.today/src/App.js
@@ -23,51 +23,51 @@ class App extends Component {
     this.scrollToBottom = this.scrollToBottom.bind(this)
 
     this.closeLightbox = this.closeLightbox.bind(this)
-		this.gotoNext = this.gotoNext.bind(this)
-		this.gotoPrevious = this.gotoPrevious.bind(this)
-		this.gotoImage = this.gotoImage.bind(this)
-		this.handleClickImage = this.handleClickImage.bind(this)
-		this.openLightbox = this.openLightbox.bind(this)
+    this.gotoNext = this.gotoNext.bind(this)
+    this.gotoPrevious = this.gotoPrevious.bind(this)
+    this.gotoImage = this.gotoImage.bind(this)
+    this.handleClickImage = this.handleClickImage.bind(this)
+    this.openLightbox = this.openLightbox.bind(this)
   }
 
   openLightbox (index, event) {
-		event.preventDefault()
-		this.setState({
-			currentImage: index,
-			lightboxIsOpen: true,
-		})
-	}
+    event.preventDefault()
+    this.setState({
+      currentImage: index,
+      lightboxIsOpen: true,
+    })
+  }
 
-	closeLightbox () {
-		this.setState({
-			currentImage: 0,
-			lightboxIsOpen: false,
-		})
-	}
+  closeLightbox () {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    })
+  }
 
-	gotoPrevious () {
-		this.setState({
-			currentImage: this.state.currentImage - 1,
-		})
-	}
+  gotoPrevious () {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    })
+  }
 
-	gotoNext () {
-		this.setState({
-			currentImage: this.state.currentImage + 1,
-		})
-	}
+  gotoNext () {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    })
+  }
 
-	gotoImage (index) {
-		this.setState({
-			currentImage: index,
-		})
-	}
+  gotoImage (index) {
+    this.setState({
+      currentImage: index,
+    })
+  }
 
-	handleClickImage () {
-		if (this.state.currentImage === this.props.images.length - 1) return
+  handleClickImage () {
+    if (this.state.currentImage === this.props.images.length - 1) return
 
-		this.gotoNext()
-	}
+    this.gotoNext()
+  }
 
   setData (result) {
     const { nextHref } = this.state
@@ -161,10 +161,10 @@ class App extends Component {
             currentImage={this.state.currentImage}
             images={images}
             isOpen={this.state.lightboxIsOpen}
-  					onClickImage={this.handleClickImage}
-  					onClickNext={this.gotoNext}
-  					onClickPrev={this.gotoPrevious}
-  					onClickThumbnail={this.gotoImage}
+            onClickImage={this.handleClickImage}
+            onClickNext={this.gotoNext}
+            onClickPrev={this.gotoPrevious}
+            onClickThumbnail={this.gotoImage}
             onClose={this.closeLightbox}
           />
           {
